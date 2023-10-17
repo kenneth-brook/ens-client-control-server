5359f666018f829d5fb5c530686fb083fd255e98
Kenneth Brook <kenneth.brook@hotmail.com>
2023-10-17 17:00:40 -0400

    started client creation endpoint

--- queries.js
+++ queries.js
@@ -26,7 +26,19 @@ const getClientsById = (request, response) => {
     })
 }
 
+const createClient = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO clients (name, email) VALUES ($1, $2) RETURNING *', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`User added with ID: ${results.rows[0].id}`)
+  })
+}
+
 module.exports = {
     getClients,
     getClientsById,
+    createClient,
   }
